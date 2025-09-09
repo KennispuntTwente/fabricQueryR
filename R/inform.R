25a8fe6f3fd1f3ad2fdbdce6260f5d13fa832db5
lukakoning <102363211+lukakoning@users.noreply.github.com>
2025-09-09 12:19:00 +0200

    Fix issue with centralized inform function
    - Could not find variables from parent env

--- R/inform.R
+++ R/inform.R
@@ -1,16 +1,20 @@
 inform <- function(
-  verbose,
-  msg,
-  type = c("info", "warning", "danger", "success")
+    verbose,
+    msg,
+    type = c("info", "warning", "danger", "success")
 ) {
   if (!isTRUE(verbose)) return(invisible())
   type <- match.arg(type)
+
+  # evaluate { } in the caller of `inform()`
+  .envir <- rlang::caller_env()
+
   switch(
     type,
-    info = cli::cli_alert_info(msg),
-    warning = cli::cli_alert_warning(msg),
-    danger = cli::cli_alert_danger(msg),
-    success = cli::cli_alert_success(msg)
+    info    = cli::cli_alert_info(msg, .envir = .envir),
+    warning = cli::cli_alert_warning(msg, .envir = .envir),
+    danger  = cli::cli_alert_danger(msg, .envir = .envir),
+    success = cli::cli_alert_success(msg, .envir = .envir)
   )
   invisible()
 }
